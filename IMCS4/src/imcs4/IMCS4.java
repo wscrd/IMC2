9a8666446f92d0a018ad553ce04f53d2518f8619
Owner <Owner@Owner-VAIO>
2015-03-31 11:37:34 +0900

    Este codigo mostra a mudanca

---
+++ IMCS4/src/imcs4/IMCS4.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imcs4;
+
+/**
+ *
+ * @author Owner
+ */
+public class IMCS4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int k = 0 ;
+        System.out.println(k);
+    }
+    
+}
