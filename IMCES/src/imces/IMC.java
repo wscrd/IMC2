842741b61070ea3ab9fbaa471fa93106b9ce59c9
Owner <Owner@Owner-VAIO>
2015-03-31 11:48:27 +0900

    Agora consegui

---
+++ IMCES/src/imces/IMC.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imces;
+
+/**
+ *
+ * @author Owner
+ */
+public class IMC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Criação de um objeto ex da classe Ex2
+        Ex2 ex = new Ex2();
+        //posição da tela ao abri-la
+        ex.setLocation(400, 200);
+        //tornando a tela visível
+        ex.setVisible(true);
+    }
+    
+}
